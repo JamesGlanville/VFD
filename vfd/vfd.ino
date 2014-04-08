427b07757ad5e57635065446bb078297e5275474
James Glanville <james.n.glanville@gmail.com>
2014-04-08 07:33:35 +0100

    Added temperature sensor and button

--- vfd/vfd.ino
+++ vfd/vfd.ino
@@ -10,6 +10,9 @@ int din = 6;
 int number=0;
 int place=0;
 int bleh=3;
+int button=8;
+
+volatile int waspressed=0;
 
 volatile int hour=0;
 volatile int minute=0;
@@ -24,7 +27,7 @@ void setup() {
   RTC.begin();
   if (0==1){
     DateTime now = DateTime(__DATE__, __TIME__);
-    DateTime future(now.unixtime()+12+3600-3600);
+    DateTime future(now.unixtime()+12+3600);
     RTC.adjust(future);
     RTC.setSqwOutLevel(HIGH);
     RTC.setSqwOutSignal();
@@ -33,6 +36,16 @@ void setup() {
   pinMode(clk, OUTPUT);     
   pinMode(din, OUTPUT); 
   pinMode(bleh, OUTPUT); 
+  
+  analogReference(EXTERNAL);
+  analogRead(0);
+  analogRead(0);
+  analogRead(0);
+  analogRead(0);
+  analogRead(0);
+
+  pinMode(button, INPUT_PULLUP); 
+
   digitalWrite(load,LOW);  
   digitalWrite(clk,LOW);  
   digitalWrite(din,LOW);  
@@ -87,38 +100,69 @@ void tick(){
     disp[8]=seconds%10;
 }
 void loop() {
-  //if(millis()%1000==0){getRTCtime();}
-  for (int i=0;i<9;i++)
+  static int temperaturereading;
+  float temp;
+  temp=0;
+  
+  int t_tens;
+  int t_ones;
+  int t_tenths;
+  
+  
+  if(!digitalRead(button))
   {
-    RefreshIV18(disp[i],i);
+    if(!waspressed)
+    {
+      waspressed=1;
+    for(int i=0;i<100;i++)
+    {
+      temp += analogRead(0);
+    }
+  temperaturereading=(int)(temp/100.0);
+  
+  }
+  float temperature = ((temperaturereading*0.4882825))-273.15;  
+  
+  t_tens=((int)(temperature/10))%10;
+  t_ones=((int)temperature)%10;
+  t_tenths=((int)(temperature*10))%10;
+  
+  if(t_tenths>=7){t_ones++;t_tenths=0;}
+  else if(t_tenths<=3){t_tenths=0;}
+  else {t_tenths=5;}
+  
+    RefreshIV18(t_tens,2,0);
+    RefreshIV18(t_ones,3,1);
+    RefreshIV18(t_tenths,4,0);
+    RefreshIV18(12,6,0);
+    RefreshIV18(13,7,0);
   }
   
-/*  if ( Serial.available() )
+  else
   {
-    char c = toupper(Serial.read());
-
-    if ( c == 'T')
+    waspressed=0;
+    for (int i=0;i<9;i++)
     {
-      hour = 10 * (Serial.read()-'0');
-      hour += Serial.read()-'0';
-      minute = 10 * (Serial.read()-'0');
-      minute += Serial.read()-'0';
-      seconds = 10 * (Serial.read()-'0');
-      seconds += Serial.read()-'0';
+      RefreshIV18(disp[i],i,0);
     }
-  }*/
+  }
+
 }
 
-void RefreshIV18(int number,int place) //decimal point is out14 (i think, wibbly bottom one)
+void RefreshIV18(int number,int place, int dot) //decimal point is out14 (i think, wibbly bottom one)
 { 
   long data=0;
   long fuck=262144L;
   long segments[]={
-    907,258,654,782,263,781,909,266,911,271,4,1<<5      }; // 0->9, then -(dash), then dot
+    907,258,654,782,263,781,909,266,911,271,4,1<<5,15,649      }; // 0->9, then -(dash), then dot
   long places[]={
     0,16,1<<14,1<<13,1<<15,1<<11,0,1024,0      };// left->right
   data^=segments[number];
   data^=places[place];
+  if(dot)
+  {
+   data|=(1<<5); 
+  }
 
   digitalWrite(load, LOW); 
 
